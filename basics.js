049c4e75f7b3268b23452c69a11052bceefba886
Brendan Hazama <brenhazama@gmail.com>
2017-08-12 15:08:29 -1000

    COMPLETE!

--- basics.js
+++ basics.js
@@ -1,5 +1,6 @@
 /* Create a `myName` variable and assign it a String value */
 var myName = "Brendan";
+console.log(myName);
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
@@ -9,6 +10,7 @@ var myName = "Brendan";
   name: "Brendan",
   age: 26,
  };
+ console.log(person);
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
@@ -27,12 +29,25 @@ console.log(canDrive);
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
-
+ function greet(name){
+  console.log("Hello, my name is " + name);
+ }
+ greet(myName);
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+ var dataTypes = [22, "Spooky Ghost", null, undefined, true, {name: myName}];
+ console.log(dataTypes);
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+ var dog = {
+  name: "Spot",
+  bark: function bark(){
+    console.log("bark!");
+  }
+ };
+
+ console.log(dog.name + " says " + dog.sound("bark!"));
